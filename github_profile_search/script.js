43d9d03e366aa6f7b55dfd6844ec7e1ce5b0c206
muzeffertagiyev <muzaffar.taghiyev@gmail.com>
2025-11-02 23:52:34 +0200

    dynamic profile card finished

--- github_profile_search/script.js
+++ github_profile_search/script.js
@@ -1,6 +1,7 @@
 const APIURL = 'https://api.github.com/users/'
 
 const form = document.getElementById('form')
+const main = document.getElementById('main')
 const search = document.getElementById('search')
 
 async function getUser(username){
@@ -8,11 +9,39 @@ async function getUser(username){
     try{
         const {data} =  await axios(APIURL+username)
 
-        console.log(data);
+        createUserCard(data)
 
     }catch(err){
-        console.log(err);
+        createErrorCard()
     }
+}
+
+function createUserCard(user){
+    const cardHTML = `
+        <div class="card">
+            <div>
+                <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+            </div>
+
+            <div class="user-info">
+                <h2>${user.name}</h2>
+                <p>${user.bio}</p>
+
+                <ul>
+                    <li>${user.followers}<strong>Followers</strong></li>
+                    <li>${user.following} <strong>Following</strong></li>
+                    <li>${user.public_repos} <strong>Repos</strong></li>
+                </ul>
+
+                <div id="repos">
+
+                    <a href="#" class="repo">Repo 1</a>
+                </div>
+            </div>
+        </div>
+        `
+
+    main.innerHTML = cardHTML
 }
 
 form.addEventListener('submit',(e)=>{
@@ -25,5 +54,5 @@ form.addEventListener('submit',(e)=>{
 
         search.value = ''
     }
-    
+
 })
