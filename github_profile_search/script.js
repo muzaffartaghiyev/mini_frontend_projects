b5d76963363a9abcbf9020c933748476ee708b20
muzeffertagiyev <muzaffar.taghiyev@gmail.com>
2025-11-02 23:37:44 +0200

    fetching data

---
+++ github_profile_search/script.js
@@ -0,0 +1,16 @@
+const APIURL = 'https://api.github.com/users/'
+
+
+getUser("muzaffar")
+
+async function getUser(username){
+
+    try{
+        const {data} =  await axios(APIURL+username)
+
+        console.log(data);
+
+    }catch(err){
+        console.log(err);
+    }
+}
