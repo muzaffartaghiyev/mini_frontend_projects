7f66c809efa1c252e505a9f3b171eea178a0507e
muzeffertagiyev <muzaffar.taghiyev@gmail.com>
2025-11-02 23:42:17 +0200

    searching user

--- github_profile_search/script.js
+++ github_profile_search/script.js
@@ -1,7 +1,7 @@
 const APIURL = 'https://api.github.com/users/'
 
-
-getUser("muzaffar")
+const form = document.getElementById('form')
+const search = document.getElementById('search')
 
 async function getUser(username){
 
@@ -14,3 +14,16 @@ async function getUser(username){
         console.log(err);
     }
 }
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+    const user = search.value
+
+    if(user){
+        getUser(user)
+
+        search.value = ''
+    }
+    
+})
