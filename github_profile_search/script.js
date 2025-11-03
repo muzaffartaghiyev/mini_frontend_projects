94529d6e1cc87e5bb9aac3408c1e5c0bc943d86b
muzeffertagiyev <muzaffar.taghiyev@gmail.com>
2025-11-03 23:47:14 +0200

    project finished

--- github_profile_search/script.js
+++ github_profile_search/script.js
@@ -24,7 +24,7 @@ async function getUser(username){
 
 async function getRepos(username){
     try{
-        const {data} =  await axios(APIURL+username + "/repos")
+        const {data} =  await axios(APIURL+username + "/repos?sort=created")
 
         addReposToCard(data)
         
